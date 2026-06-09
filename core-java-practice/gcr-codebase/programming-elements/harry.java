565600260227a3352ece71c483370595e934649e
Sambhav Varshney <sambhav.varshney_cs23@gla.ac.in>
2026-06-09 10:38:41 +0530

    level1-Q-1 by sambhav

---
+++ core-java-practice/gcr-codebase/programming-elements/harry.java
@@ -0,0 +1,8 @@
+class harry{
+    public static void main(String args[]){
+        int brthyear=2000;
+        int currentyr=2024;
+        int age=currentyr-brthyear;
+        System.out.println("Harry age is"+age);
+    }
+}
