a1d1245393f0d5a10a6e0a4bbde81673e8ad90d8
Sambhav-Varshney <sambhav.varshney_cs23@gla.ac.in>
2026-06-15 23:31:47 +0530

    Delete core-java-practice/gcr-codebase directory

--- core-java-practice/gcr-codebase/programming-elements/harry.java
+++
@@ -1,8 +0,0 @@
-class harry{
-    public static void main(String args[]){
-        int brthyear=2000;
-        int currentyr=2024;
-        int age=currentyr-brthyear;
-        System.out.println("Harry age is"+age);
-    }
-}
